73c74532f187a5edf9f20b554f576d98f0060a0f
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-01-16 21:35:56 +0100

    Correct

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str)
+    for i in range(len(str)):
+        n = ord(str[i])
+        if n >= 97 and n <= 112:
+            n = n - 32
+        print("{:c}".format(temp), end='')
+    print()
