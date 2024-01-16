07df0973305d4a98695d6218a9d317ad8a27ac2e
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-01-16 21:46:56 +0100

    Correct

--- 0x01-python-if_else_loops_functions/6-print_combo3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(0, 10):
-        if j - 1 <= 0:
-            continue
-        print("{:d}{:d}".format(i, j), end = ", " if i != 8 else '')
-print()
