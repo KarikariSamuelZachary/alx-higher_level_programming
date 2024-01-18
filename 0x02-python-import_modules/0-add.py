6bb193337cebecaf3eb2b1e5801f79511b9e5b2f
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-01-18 20:36:33 +0100

    Correct:

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-num = add(a, b)
-print("{0:d} + {1:d} = {2:d}".format(a, b, num))
+if __name__ == '__main__':
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
