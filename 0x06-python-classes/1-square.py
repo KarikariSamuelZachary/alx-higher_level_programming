b7f753438d4c4ef39ff3e60a49f462d56eee2787
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-02-07 01:15:07 +0100

    Python Docstring fix

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+Square module
+
+This module contains finction def for sq
+
+
+"""
+
+
+class Square():
+    """Sqare Definition"""
+
+    def __init__(self, size):
+        """
+        Constructor Definition
+
+        Args:
+            size(int): size of one side of the square
+        """
+        self.__size = size
