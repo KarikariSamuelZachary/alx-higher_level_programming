bed263bef094299e7ccd205e59a961612b0933f2
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-02-07 01:55:52 +0100

    Fixed

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,7 +10,7 @@ This module contains finction def for sq
 
 
 class Square():
-    """Sqare Definition"""
+    """Class Square Definition"""
 
     def __init__(self, size=0):
         """
@@ -31,3 +31,13 @@ class Square():
                 self.__size = size
         else:
             raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        Area Function Definition
+
+        Args:
+            size(int): size of one side of the square
+        """
+
+        return self.__size ** 2
