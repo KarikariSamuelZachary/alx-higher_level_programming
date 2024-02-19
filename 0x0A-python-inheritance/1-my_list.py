ee729f3e513bb6f48bfc2cba381b1ed91e598905
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-02-19 22:52:15 +0100

    Added task

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""My list module
+
+Contans a subclass My List that inherits
+from the Base Class List
+
+
+"""
+
+
+class MyList(list):
+    """Defines the MyList Class"""
+
+    def __init__(self):
+        pass
+
+    def print_sorted(self):
+        """Prints the list but in sorted ascending order"""
+        print(sorted(self))
