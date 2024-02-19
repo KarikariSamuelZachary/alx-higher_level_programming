87988864d2a68580048156cd6103a62b5266ea1b
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-02-20 00:38:11 +0100

    Made changes

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This inherits from the rectagle class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    Attributes:
+    size: size of square
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ calculates the area """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ print object """
+        return "[Square] {}/{}".format(self.__size, self.__size)
