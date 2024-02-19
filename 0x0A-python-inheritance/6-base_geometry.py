10f7fada913fa83756e9211d37b7491c8c55a0cc
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-02-19 23:58:56 +0100

    Made changes

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,7 +7,7 @@ Contains a class with a public method
 
 class BaseGeometry:
     """Defines BaseGeometry Class"""
-    
+
     def area(self):
         """Raises an exception"""
         raise Exception("area() is not implemented")
