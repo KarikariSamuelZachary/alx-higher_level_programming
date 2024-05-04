f48fd2ddb0eea7f726adc471c4b5ca6f201107f8
KarikariSamuelZachary <skarikaripresecghana@gmail.com>
2024-05-04 01:18:10 +0000

    Added task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-This script lists all values in the `states` table of `hbtn_0e_0_usa`
-where `name` matches the argument `state name searched`.
+This script that lists all `states` with a name starting
+with `N` from database `hbtn_0e_0_usa`.
 
 Arguments:
     mysql username (str)
     mysql password (str)
     database name (str)
-    state name searched (str)
 """
 
 import sys
@@ -16,16 +15,10 @@ import MySQLdb
 if __name__ == "__main__":
     username, passw, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    searched_name = sys.argv[4]
-
     db = MySQLdb.connect(user=username, passwd=passw, db=db_name)
     cur = db.cursor()
 
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id".format(
-            searched_name
-        )
-    )
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     rows = cur.fetchall()
 
     for row in rows:
